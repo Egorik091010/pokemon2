07cf65acc4aa673c57ac0d9da0b2440cf3e76d48
egor <moiseenkovegorik09@gmail.com>
2025-03-11 17:46:00 +0100

    Pokemon

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-token = ""
+token = "7604456648:AAEz_h7Tzwm2d6MK5QcSRcXOcyXXyxCUjwU"
--- logic.py
+++ logic.py
@@ -11,13 +11,18 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
-
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
     def get_img(self):
-        pass
-    
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['front_default'])
+        else:
+            return "Pikachu"
+
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -36,6 +41,12 @@ class Pokemon:
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
+
+    
+
+    
+
 
 
 
